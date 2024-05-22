74bdbd310c288bd0dd5c2646a1d75ee8bef31588
Nicole <nickii.cn@hotmail.com>
2024-05-22 16:43:18 -0300

    mongoose

--- db.js
+++ db.js
@@ -1,6 +1,13 @@
 // db.js
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb+srv://nickiicn:<password>@cluster0.sxitpsr.mongodb.net/');
+mongoose.connect('mongodb+srv://nickiicn:<nicole1306>@cluster0.sxitpsr.mongodb.net/ecommercedb?retryWrites=true&w=majority', 
+{ useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log("Conexión exitosa");
+});
 
 export default mongoose;
--- src/app.js
+++ src/app.js
@@ -11,9 +11,13 @@ import cartRouter from "./routes/carts.router.js"
 
 const app = express()
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://nickiicn:<nicole1306>@cluster0.sxitpsr.mongodb.net/');
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use('mongodbVSCodePlaygroundDB');
 
 //app.use(express.static(__dirname + "/public"))
 
