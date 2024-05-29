124541a25f4a183ab68eed40e57f76ecf98887e5
Nicole <nickii.cn@hotmail.com>
2024-05-29 18:04:22 -0300

    integracion bbd y loguin

--- src/routes/view.router.js
+++ src/routes/view.router.js
@@ -16,4 +16,30 @@ router.get("/realtimeproducts", async (req, res) => {
     res.render("realTimeProducts")
 })
 
+
+router.get("/carts/:cid", async (req, res) => { 
+    const { cid } = req.params
+    const carrito = await pmanager.getCartbyId(req.params)
+    res.render("carts", { carrito })
+})
+
+router.get("/loguin", async (req, res) => {
+    res.render("login")
+})
+
+router.get("/register", async (req, res) => {
+    res.render("register")
+})
+
+router.get("/chat", async (req, res) => {
+    res.render("chat")
+})
+
+router.get("/logout", async (req, res) => { 
+    res.render("logout")
+})
+
+
+
+
 export default router
