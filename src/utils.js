4d874a490564d845db40c09cf7512ca422d03295
Nicole <nickii.cn@hotmail.com>
2024-06-06 12:08:50 -0300

    authenticacion y correcciones

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,6 @@
 import multer from 'multer';
 import config from './config.js';
+import bcrypt from 'bcrypt';
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -11,5 +12,21 @@ const storage = multer.diskStorage({
         cb(null, file.originalname)
     }
 });
+
+
+export const createHash = password => bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+export const isValidPassword = (passwordToVerify, storedHash) => bcrypt.compareSync(passwordToVerify, storedHash);
+
+export const verifyRequiredBody = (requiredFields) => {
+    return (req, res, next) => {
+        const allOk = requiredFields.every(field =>
+            req.body.hasOwnProperty(field) && req.body[field] !== '' && req.body[field] !== null && req.body[field] !== undefined
+        );
+
+        if (!allOk) return res.status(400).send({ origin: config.SERVER, payload: 'Faltan propiedades', requiredFields });
+
+        next();
+    };
+};
 
 export const uploader = multer({ storage: storage });
